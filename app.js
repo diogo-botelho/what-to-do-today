5bbc37b213a3d1806b1f54338a6a0444293e50c6
Diogo <diogofilipebotelho@gmail.com>
2022-09-22 21:24:53 -0400

    building the bones of the app

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
-app.listen(3000, function () {
-  console.log("App started at http://localhost:3000/");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    return res.send("Hello World!");
 });
+
+app.get("/staff/:fname", function (req, res) {
+    return res.send(`This instructor is ${req.params.fname}`);
+});
+
+  
+module.exports = app;
